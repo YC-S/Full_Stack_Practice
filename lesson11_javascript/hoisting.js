f5e85d98a15748a3f15f25d30d938fbc3fb50e38
yuanchen <yuanchenshi@gmail.com>
2020-02-01 16:22:09 -0600

    hoisting

---
+++ lesson11_javascript/hoisting.js
@@ -0,0 +1,32 @@
+test();
+
+function test() {
+  console.log(1);
+}
+
+// console.log(c);
+
+let c;
+c = 10;
+console.log(c);
+
+function f1() {
+  console.log('f1');
+  f2();
+}
+
+function f2() {
+  console.log('f2');
+  f3();
+}
+
+function f3() {
+  console.log('f3');
+  f4();
+}
+
+function f4() {
+  console.log('f4');
+}
+
+f1();
