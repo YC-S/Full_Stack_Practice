aaa8a4a3227f35e3dffd0bfdad86e12565067d76
yuanchen <yuanchenshi@gmail.com>
2020-02-02 17:03:42 -0600

    lesson12

--- lesson11_javascript/hoisting.js
+++ lesson11_javascript/hoisting.js
@@ -1,3 +1,6 @@
+//Hoisting
+// Hoisting is a JavaScript mechanism where variables and function declarations are moved to the top of
+// their scope before code execution.
 test();
 
 function test() {
